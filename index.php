4cf10116c030ac9cb66f5345caf18ea1201fac23
SierikovRoman <rebit1love@gmail.com>
2017-06-14 00:07:20 +0300

    some changes for heroku

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <script src="app/libs/angular/angular-route.min.js"></script>
     <script src="app/libs/bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="app/js/script.js"></script>
-    <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+    <!-- <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script> -->
 </head>
 <body>
 
