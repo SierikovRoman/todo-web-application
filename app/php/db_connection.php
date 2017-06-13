4a99ff3333732098446104d6bd96dab53d1bb44e
SierikovRoman <rebit1love@gmail.com>
2017-06-14 00:03:50 +0300

    add new db_connection

--- app/php/db_connection.php
+++ app/php/db_connection.php
@@ -2,11 +2,11 @@
 // Connecting to database as mysqli_connect("hostname", "username", "password", "database name");
 
 $con = pg_connect("
-	host=ec2-23-23-93-255.compute-1.amazonaws.com 
+	host=ec2-107-21-99-176.compute-1.amazonaws.com 
 	port=5432 
-	dbname=d4534uc1p8js9a 
-	user=qjbfryfoxhmsdp 
-	password=6fb532e056f5eeee54b4605a07183364eb1b17eda436fb19716d822942b7d772
+	dbname=d3c61t0ne622qg 
+	user=vusnbpashokbzw 
+	password=c383cb26bab0ebb3e8fa35ec2e0af395377952cfc9f48013e852c280c4f104e1
 	") or die("Could not connect" . pg_last_error());
 
 // $stat = pg_connection_status($con);
