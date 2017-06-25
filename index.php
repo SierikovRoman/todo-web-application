ac9407d48abbf3a1728a7bb5dcab642426de2aea
SierikovRoman <rebit1love@gmail.com>
2017-06-25 12:27:06 +0300

    testing PWA

--- index.php
+++ index.php
@@ -13,6 +13,15 @@
     <script src="app/libs/bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="app/js/script.js"></script>
     <!-- <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script> -->
+    <!-- TODO add manifest here -->
+      <link rel="manifest" href="/manifest.json">
+      <!-- Add to home screen for Safari on iOS -->
+      <meta name="apple-mobile-web-app-capable" content="yes">
+      <meta name="apple-mobile-web-app-status-bar-style" content="black">
+      <meta name="apple-mobile-web-app-title" content="Todo PWA">
+      <link rel="apple-touch-icon" href="app/images/icons/icon-152x152.png">
+      <meta name="msapplication-TileImage" content="app/images/icons/icon-144x144.png">
+      <meta name="msapplication-TileColor" content="#2F3BA2">
 </head>
 <body>
 
